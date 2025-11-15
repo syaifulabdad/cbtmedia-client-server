c0d3e948c048ea151ca07f60341f216c02ab8698
Aplikasimedia <syaifulabdad@gmail.com>
2025-11-16 06:46:27 +0700

    Update 2024_01_07_051622_tarik_data.php

--- database/migrations/2024_01_07_051622_tarik_data.php
+++ database/migrations/2024_01_07_051622_tarik_data.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('tarik_data', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id();
             $table->uuid('sekolah_id')->nullable()->index();
             $table->string('nama');
             $table->string('host')->nullable();
